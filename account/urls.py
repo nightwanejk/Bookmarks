94a57765722dd99015c049ea00ed3e44ffe0baa0
nightwanejk <hellicformone2015@yandex.ru>
2023-07-08 21:19:39 +0300

    Bookmarks + Social Auth Google

---
+++ account/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login')
+    # path('login/', views_auth.LoginView.as_view(), name='login'),
+    # path('logout/', views_auth.LogoutView.as_view(), name='logout'),
+    #
+    # path('password-change/', views_auth.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', views_auth.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #
+
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
