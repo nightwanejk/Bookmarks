ac44fc30cb0b1d46402b3ce706d0b0740e125fe7
nightwanejk <hellicformone2015@yandex.ru>
2023-07-09 22:54:54 +0300

    Image url

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -18,3 +21,17 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not match valid extensions.')
 
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # скачать изображение с данного URL-адреса
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
