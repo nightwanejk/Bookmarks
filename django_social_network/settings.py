513c3f4a064e8f709d56cfa2389f72bc8a24a9d2
nightwanejk <hellicformone2015@yandex.ru>
2023-07-15 06:22:15 +0300

    add redis via remote access(virtualbox/ubuntu)

--- django_social_network/settings.py
+++ django_social_network/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django_redis.cache import RedisCache
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
@@ -184,6 +186,16 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-REDIS_HOST = 'localhost'
+REDIS_HOST = '192.168.0.8'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://<192.168.0.8>:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
